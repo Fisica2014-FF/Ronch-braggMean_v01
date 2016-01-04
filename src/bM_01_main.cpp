938c3c4e88e4728b9edb660127a706963047d7fc
Fisica2014-FF <directoryh22@gmail.com>
2016-01-04 22:16:53 +0100

    Finito, da rivedere per meettere i commenti.

--- src/bM_01_main.cpp
+++ src/bM_01_main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 #include "dbg_macro.h"
 #include "Event.h"
@@ -15,7 +16,7 @@
 using namespace std;
 
 //const has static linkage by default, so we must use explicitly extern here.
-extern const unsigned int MIN_POINTS = 120;
+extern const unsigned MIN_POINTS = 120;
 
 void clear(Event* ev);
 
@@ -24,12 +25,16 @@ Event* read(std::ifstream& ifs);
 void dump(const Event& ev);
 
 bool add(const Event& ev, long sumenergies[], unsigned long sumsquares[],
-		long min_en, long max_en);
+		int min_en, int max_en);
+
+void stat(const unsigned numevents, const unsigned numpoints,
+		long sumenergies[], unsigned long sumsquares[], double meanenergies[],
+		double rmsenergies[]);
 
 int main(int argc, char* argv[]) {
 	using namespace std;
 
-	//Read file name, see DBG macro at the start.
+	//Read file name, see DBG macro
 	DBG(const char* filename = "./bragg_events.txt";
 		, const char* filename = argv[1];)
 
@@ -37,25 +42,45 @@ int main(int argc, char* argv[]) {
 	ifstream ifs(filename);
 	long* sumenergies = new long[MIN_POINTS];
 	unsigned long* sumsquares = new unsigned long[MIN_POINTS];
-	double* meanenergy;
-	double* rmsenergy;
+	double* meanenergies = new double[MIN_POINTS];
+	double* rmsenergies = new double[MIN_POINTS];
 
 	//Inizialize at zero the arrays.
 	for (unsigned i = 0; i < MIN_POINTS; ++i) {
 		sumenergies[i] = 0;
-		sumsquares = 0;
+		sumsquares[i] = 0;
 	}
 
-	/*
-	 Event* event;
-	 while (ifs) {
-	 event = read(ifs);
-	 if (event) {
-	 dump(*event);
-	 clear(event);
-	 }
-	 }*/
+	Event* event;
+	bool is_in_range;
+	unsigned counter_valid_events = 0;
+	//Read the file and do the statistics.
+	while (ifs) {
+		event = read(ifs);
+		if (event) {
+			is_in_range = add(*event, sumenergies, sumsquares, 6000, 7500);
+			//If event is in range increment the counter.
+			if (is_in_range) {
+				++counter_valid_events;
+			}
+			clear(event);
+		}
+	}
 
+	stat(counter_valid_events, MIN_POINTS, sumenergies, sumsquares,
+			meanenergies, rmsenergies);
+
+	//Print the four arrays
+	cout << "Point     TotEnergy  TotSquares MeanEnergy     RMS" << endl;
+	for (unsigned i = 0; i < MIN_POINTS; ++i) {
+		printf("%9u %9i  %9u  %9f %9f",i+1,sumenergies[i],sumsquares[i],meanenergies[i],rmsenergies[i]);
+		cout << endl;
+	}
+
+	delete[] sumenergies;
+	delete[] sumsquares;
+	delete[] meanenergies;
+	delete[] rmsenergies;
 	return 0;
 }
 
